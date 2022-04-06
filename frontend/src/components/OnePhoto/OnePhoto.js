6bc7b1d5771e4d94d1031be739e9b3df5d0652b6
Huyen Nguyen <hn078722@gmail.com>
2022-04-05 20:19:02 -0700

    delete feature with redirect

--- frontend/src/components/OnePhoto/OnePhoto.js
+++ frontend/src/components/OnePhoto/OnePhoto.js
@@ -5,12 +5,14 @@ import { getOne} from "../../store/upload"
 import { NavLink } from "react-router-dom"
 import "./OnePhoto.css"
 import { deletingOne } from "../../store/upload"
+import { useHistory } from "react-router-dom"
 
 
 export default function OnePhoto () {
-
+    const history = useHistory()
     const dispatch = useDispatch()
     const {imageId} = useParams()
+    const sessionUser = useSelector(state => state.session.user);
 
 
     const photos = useSelector((state) => {
@@ -18,16 +20,24 @@ export default function OnePhoto () {
     return state.upload[imageId]
     })
 
+    if (!sessionUser) {
+        history.push("/")
+
+    }
+
     // const one = {};
     // photos.forEach((photo) => {
     //   one[photo.id] = photo;
     // });
+    
+
 
    //console.log(one)
 
     // console.log(currentOne)
     
     //console.log(onePhoto)
+
 
     useEffect(() => {
     dispatch(getOne(imageId))
@@ -42,7 +52,10 @@ export default function OnePhoto () {
           <NavLink to={`/photos/${imageId}/edit`}>
           <button>Edit</button>
           </NavLink>
-          <button onClick={() => dispatch(deletingOne(imageId))}>Delete</button>
+          <button onClick={() => {
+              dispatch(deletingOne(imageId))
+              return history.push("/home")
+            }}>Delete</button>
           
                 
         </div>
