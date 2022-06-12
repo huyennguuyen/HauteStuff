ef9d1d67327054816af8fdca4a5b7099bce42d81
huyennguuyen <92718001+huyennguuyen@users.noreply.github.com>
2022-06-11 20:42:24 -0700

    changed splash page look

--- frontend/src/components/Navigation/DemoButton.js
+++ frontend/src/components/Navigation/DemoButton.js
@@ -1,10 +1,13 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {setUser} from "../../store/session"
 import * as sessionActions from '../../store/session';
+import { useHistory } from "react-router-dom";
+import { Redirect } from 'react-router-dom';
 import { useState } from 'react';
 
 
 export default function DemoButton () {
+const history = useHistory()
 const dispatch = useDispatch()
 
 const user = {     
@@ -14,11 +17,16 @@ const user = {
 
 
 
-const submitting = (e) => {
+
+const submitting = async (e) => {
     e.preventDefault()
    
 
-    return dispatch(sessionActions.login(user))
+   await dispatch(sessionActions.login(user))
+
+    history.push("/home")
+
+
 }
 
 
