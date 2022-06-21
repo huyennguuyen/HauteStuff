03d5a0e51d50108088b5cd366a8a39923943177c
huyennguuyen <92718001+huyennguuyen@users.noreply.github.com>
2022-06-20 23:39:05 -0700

    added in editorial for right side

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -16,6 +16,7 @@ export default function Homepage () {
     const dispatch = useDispatch()
     const {imageId} = useParams()
     const [image, setImage] = useState("")
+    const [image2, setImage2] = useState("")
     // const [users, setUsers] = useState("")
 
     // const photos = useSelector(state => {
@@ -33,20 +34,6 @@ export default function Homepage () {
 
     }
 
-    // useEffect(() => {
-    //     async function fetchData() {
-    //       const response = await fetch('/api/users/all');
-    //       const responseData = await response.json();
-    //       const one = {};
-    //       responseData.forEach((user) => {
-    //           one[user.id] = user;
-    //         });
-            
-    //     console.log("THIS IS response DATA-----", one)
-    //       setUsers(one);
-    //     }
-    //     fetchData();
-    //   }, []);
 
 
     const one = {};
@@ -75,13 +62,23 @@ export default function Homepage () {
         dispatch(loadAllUsers())
     }, [dispatch])
 
+    
     const random = Math.floor(Math.random() * photos.length);
+    const random1 = Math.floor(Math.random() * photos.length);
+    const random2 = Math.floor(Math.random() * photos.length);
+    const random3 = Math.floor(Math.random() * photos.length);
+    const random4 = Math.floor(Math.random() * photos.length);
+    const random5 = Math.floor(Math.random() * photos.length);
+    const random6 = Math.floor(Math.random() * photos.length);
+    
+    // useEffect(() => {
+        
+        
+    // setImage(photos[random]?.imageUrl)
+    // // setImage2(photos[random]?.imageUrl)
 
-    useEffect(() => {
+    // })
 
-         setImage(photos[random]?.imageUrl)
-
-    })
 
 
     if(!photos) {
@@ -155,13 +152,50 @@ export default function Homepage () {
                 </div>
                 <div className="right-home">
                     <div className="random-pics">
-                        {photos.map(photo => (
-                            photo?.imageUrl === image && (
-                            <NavLink to={`/photos/${photo?.id}`}>
-                                <img src={image}></img>
+                        <h3 className="explore-header">Explore photos here</h3>
+                        <div className="pics-box">
+                            <NavLink to={`/photos/${photos[random]?.id}`} className="photo-link">
+                                <img src={photos[random]?.imageUrl} className="random-right"></img>
                             </NavLink>
-                            )
-                        ))}
+                            <NavLink to={`/photos/${photos[random1]?.id}`}className="photo-link">
+                                <img src={photos[random1]?.imageUrl} className="random-right-one"></img>
+                            </NavLink>
+                            <NavLink to={`/photos/${photos[random2]?.id}`}
+                            className="photo-link">
+                                <img src={photos[random2]?.imageUrl}
+                                className="random-right-two"></img>
+                            </NavLink>
+                            <NavLink to={`/photos/${photos[random3]?.id}`}className="photo-link">
+                                <img src={photos[random3]?.imageUrl}
+                                className="random-right-three"></img>
+                            </NavLink>
+                            <NavLink to={`/photos/${photos[random4]?.id}`}className="photo-link">
+                                <img src={photos[random4]?.imageUrl}
+                                className="random-right-four"></img>
+                            </NavLink>
+                            <NavLink to={`/photos/${photos[random5]?.id}`}className="photo-link">
+                                <img src={photos[random5]?.imageUrl}
+                                className="random-right-five"></img>
+                            </NavLink>
+                            <NavLink to={`/photos/${photos[random6]?.id}`}className="photo-link">
+                                <img src={photos[random6]?.imageUrl}
+                                className="random-right-six"></img>
+                            </NavLink>   
+                        </div>
+                    </div>
+                    <div className="editorial">
+                        <h3 className="explore-header">Haute Stuff Editorial</h3>
+                        <div className="latest-news">
+                            <img src="https://i.postimg.cc/qvMyHtqN/287253098-2718787858255885-6572968416685801941-n.jpg" className="emma"></img>
+                            <p>
+                                Ran out of clothes to wear? Don't worry, pajama sets will work just fine. This  latest trend is too good to be true, cute and comfy. Time to swap out mini bags for pillows. What do you think? Share your lastest set!
+                            </p>
+                            <a href="https://www.instagram.com/emmachamberlain/" target="_blank" rel="noopener noreferrer">
+                            <p className="emma-insta">
+                                pc: Emma Chamberlain
+                            </p>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
