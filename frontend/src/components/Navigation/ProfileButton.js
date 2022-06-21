0616721203a848bd96ebb5ab90b8ff13556b448c
huyennguuyen <92718001+huyennguuyen@users.noreply.github.com>
2022-06-21 01:13:04 -0700

    added in the user profile in nav bar and changed out the modal

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -3,7 +3,9 @@ import * as sessionActions from '../../store/session';
 import './Navigation.css';
 import {NavLink} from "react-router-dom";
 // import { oneUser } from "../../store/user"
+import { loadAllUsers } from "../../store/allUsers";
 import {ReactComponent as SVG} from "../../logo.svg";
+import Popup from "reactjs-popup"
 import {useDispatch, useSelector} from "react-redux"
 
 
@@ -11,6 +13,16 @@ function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
   // const currentUser = useSelector(state => state.user.user);
+
+  // console.log("THIS IS CURRENT USER------", currentUser)
+
+  const allUsers = useSelector(state => state.allUsers.user);
+  console.log("THIS IS ALL USERS----", allUsers)
+
+  const users = {};
+  allUsers?.forEach((user) => {
+    users[user.id] = user;
+  });
   
   const openMenu = () => {
     if (showMenu) return;
@@ -29,9 +41,10 @@ function ProfileButton({ user }) {
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
-  // useEffect( () => {
-  //   dispatch(oneUser(currentUser.id))
-  // },[currentUser, dispatch])
+    useEffect (() => {
+      dispatch(loadAllUsers())
+    },[dispatch])
+
 
   const logout = (e) => {
     e.preventDefault();
@@ -42,39 +55,36 @@ function ProfileButton({ user }) {
   return (
     <>
     <div className="together">
-      <button onClick={openMenu} className="side">
-        <i className="fas fa-user-circle" />
-      </button>
-        <div className="outsideProfile">
-          <div className="profile-dropdown">
-            {showMenu && (
-              <ul className="pro">
-                <div className="side">
-                  <div>
-                    <NavLink to={`/users/${user?.id}`}>            
-                      <li className="username">{user?.username}</li>
-                    </NavLink>
-                  </div>
-                  {/* <div className="user">
-                    <li>{user.email}</li>
-                  </div> */}
-                  <li>
-                      <NavLink to={`/users/${user?.id}/edit`}> 
-                        Settings
+            <Popup
+              trigger ={users[user?.id]?.profileUrl ? <img src={users[user?.id]?.profileUrl} onClick={openMenu} className="profile-homepage side"></img>:
+              <img src="https://cdn.myportfolio.com/0da7f5fbc31f3b0a622becb5c04363c6/ee759715-7080-4029-8458-50a20bff014c_rw_1920.jpg?h=ba7face07c8aec7970909f3eb3c91045" className="profile-homepage side" onClick={openMenu}></img>
+              }
+              position="bottom right"
+              className="foo"
+              closeOnDocumentClick
+              >
+                <ul className="pro">
+                  <div className="side">
+                    <div>
+                      <NavLink to={`/users/${user?.id}`}>            
+                        <li className="username">{user?.username}</li>
                       </NavLink>
-                    </li>
-                  <div className="user">
+                    </div>
                     <li>
-                      <NavLink exact to="/">
-                      <button onClick={logout} className="logout">Log Out</button>
-                      </NavLink>
-                    </li>
+                        <NavLink to={`/users/${user?.id}/edit`}> 
+                          Settings
+                        </NavLink>
+                      </li>
+                    <div className="user">
+                      <li>
+                        <NavLink exact to="/">
+                        <button onClick={logout} className="logout">Log Out</button>
+                        </NavLink>
+                      </li>
+                    </div>
                   </div>
-                </div>
-              </ul>
-            )}
-          </div>
-        </div>
+                </ul>
+          </Popup>
     </div>
     </>
   );
