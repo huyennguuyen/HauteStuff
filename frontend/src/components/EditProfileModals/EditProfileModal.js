f84154e4856d3ff35b2f65e9fd83774d1438d712
huyennguuyen <92718001+huyennguuyen@users.noreply.github.com>
2022-06-23 18:57:06 -0700

    fixed the cover modal

--- frontend/src/components/EditProfileModals/EditProfileModal.js
+++ frontend/src/components/EditProfileModals/EditProfileModal.js
@@ -133,50 +133,50 @@ export default function EditProfileModal ({closeModal, userId}) {
     }
 
   return (
-      <>
-       <div >
-             <div className="close-end-profile">
+    <>
+        <div>
+             <div className="close-end-profile" onClick={closeModal}>
                 <img src={cancel} className="close-pic"></img>
             </div>
-        <div >
-            <form onSubmit={handleSubmit} className="forms profile-form" id="signupForm">
-                <ul className="ul-fix">
-                    {hasSubmitted && errors.map((error, idx) => <li key={idx} className="errors ul-fix">{error}</li>)}
-                </ul>
-                <div className="typeError">
-                {typeError}
-                </div>
-                <div className="loading-text">
-                    {imageLoading && <p className="loading-upload">Loading...</p>}
-                </div>
-                <div className="drop-prof">
-                    <FileUploader
-                        onTypeError={onTypeError}
-                        handleChange={handleChange}
-                        name='image'
-                        onDraggingStateChange={onDrag}
-                        types={fileTypes}
-                    >
-                        <div className="drop-prof-inside">
-                            {profileUrl ? <img src={URL.createObjectURL(profileUrl)} alt='upload-preview' className="upload-preview-prof" onError={({ currentTarget }) => {
-                            currentTarget.onerror = null;
-                            currentTarget.src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ87Ktf3Xk1ZjtNnEV_dzJxDB0VANB8ELKAew&usqp=CAU"
-                            setTypeError("Please upload a jpg, png, gif, or jpeg file type.")
-                            }}/> : disabled && 
-                            <div className="text-image-prof"> 
-                                <h4 className="cover=uploading">Drag and drop file here/ Click to upload</h4>
-                            </div>   
-                            }
-                        </div>
-                    </FileUploader>
-                    <div className="button-cover profile-moving">
-                        <button  className="cancel-cover-button" onClick={() => closeModal()}>Cancel Changes</button>
-                        <button type="submit" className="save-cover-button">Save Changes</button>
+            <div >
+                <form onSubmit={handleSubmit} className="forms profile-form" id="signupForm">
+                    <ul className="ul-fix">
+                        {hasSubmitted && errors.map((error, idx) => <li key={idx} className="errors ul-fix">{error}</li>)}
+                    </ul>
+                    <div className="typeError">
+                    {typeError}
                     </div>
-                </div>
-            </form>
+                    <div className="loading-text">
+                        {imageLoading && <p className="loading-upload">Loading...</p>}
+                    </div>
+                    <div className="drop-prof">
+                        <FileUploader
+                            onTypeError={onTypeError}
+                            handleChange={handleChange}
+                            name='image'
+                            onDraggingStateChange={onDrag}
+                            types={fileTypes}
+                        >
+                            <div className="drop-prof-inside">
+                                {profileUrl ? <img src={URL.createObjectURL(profileUrl)} alt='upload-preview' className="upload-preview-prof" onError={({ currentTarget }) => {
+                                currentTarget.onerror = null;
+                                currentTarget.src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ87Ktf3Xk1ZjtNnEV_dzJxDB0VANB8ELKAew&usqp=CAU"
+                                setTypeError("Please upload a jpg, png, gif, or jpeg file type.")
+                                }}/> : disabled && 
+                                <div className="text-image-prof"> 
+                                    <h4 className="cover=uploading">Drag and drop file here/ Click to upload</h4>
+                                </div>   
+                                }
+                            </div>
+                        </FileUploader>
+                        <div className="button-cover profile-moving">
+                            <button  className="cancel-cover-button" onClick={() => closeModal()}>Cancel Changes</button>
+                            <button type="submit" className="save-cover-button">Save Changes</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
       </>
   )
 
