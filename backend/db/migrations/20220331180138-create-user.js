8d788242203362b6782866a39be4a08deff9d99d
huyennguuyen <92718001+huyennguuyen@users.noreply.github.com>
2022-11-24 15:25:16 -0800

    adding to the migrations file

--- backend/db/migrations/20220331180138-create-user.js
+++ backend/db/migrations/20220331180138-create-user.js
@@ -1,4 +1,10 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; 
+}
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Users', {
@@ -52,9 +58,9 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.fn('now')
       }
-    });
+    }, options);
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
+    return queryInterface.dropTable('Users', options);
   }
 };
